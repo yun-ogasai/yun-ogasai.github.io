c03e1a4ddd01e9c8300c141f9342ff0c53d0e5f9
yunogasai <yunogasai12344@outlook.com>
2024-06-30 09:48:38 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://YUN-OGASAI.github.io'
+    site:'https://yun-ogasai.github.io'
 });
