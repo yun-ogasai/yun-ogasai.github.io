b7b6ca966f56b3e5c194e1baa6793f7f3b34a88d
yunogasai <yunogasai12344@outlook.com>
2024-07-06 09:02:36 +0100

    format

--- src/components/CurrentTime.tsx
+++ src/components/CurrentTime.tsx
@@ -1,37 +1,40 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const getTimeZoneOffset = () => {
-  const offset = new Date().getTimezoneOffset();
-  const sign = offset <= 0 ? '+' : '-';
-  const hours = Math.floor(Math.abs(offset) / 60);
-  const minutes = Math.abs(offset) % 60;
-  return `UTC${sign}${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    const offset = new Date().getTimezoneOffset();
+    const sign = offset <= 0 ? "+" : "-";
+    const hours = Math.floor(Math.abs(offset) / 60);
+    const minutes = Math.abs(offset) % 60;
+    return `UTC${sign}${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}`;
 };
 
 const CurrentTime = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [timeZone, setTimeZone] = useState('');
-  const [timeZoneOffset, setTimeZoneOffset] = useState('');
-  useEffect(() => {
-    setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone);
-    setTimeZoneOffset(getTimeZoneOffset());
-    
-  });
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [timeZone, setTimeZone] = useState("");
+    const [timeZoneOffset, setTimeZoneOffset] = useState("");
+    useEffect(() => {
+        setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone);
+        setTimeZoneOffset(getTimeZoneOffset());
+    });
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
 
-    return () => clearInterval(timer);
-  });
+        return () => clearInterval(timer);
+    });
 
-  return (
-    <div>
-      <p>{currentTime.toLocaleTimeString()}</p>
-      <p>{timeZone} ({timeZoneOffset})</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>{currentTime.toLocaleTimeString()}</p>
+            <p>
+                {timeZone} ({timeZoneOffset})
+            </p>
+        </div>
+    );
 };
 
 export default CurrentTime;
